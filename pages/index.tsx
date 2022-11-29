f32836e747aa282702a0f80d1626cce699886e1a
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-11-29 18:34:07 +0500

    block rotating ability added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,36 @@
 import { NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+function AnimatedBox() {
+  const meshRef = useRef<THREE.Mesh>(null);
+  useFrame(() => {
+    console.log("Hey, I'm executing every frame!");
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} scale={[0.5, 0.5, 0.5]}>
+      <boxGeometry />
+      <meshBasicMaterial color="royalblue" />
+    </mesh>
+  );
+}
 
 const Home: NextPage = () => {
   return (
-    
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>{" "}
-    </Canvas>
+    <div className="container">
+      {" "}
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <AnimatedBox />
+      </Canvas>
+    </div>
   );
 };
 export default Home;
