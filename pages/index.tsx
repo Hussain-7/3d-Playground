dd7ab6331fbd09f4ed2353c0e1006963a824711c
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-11-30 12:46:16 +0500

    animated box component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,12 @@ import { NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import AnimatedBox from "../components/AnimatedBox";
 
-function AnimatedBox() {
-  const meshRef = useRef<THREE.Mesh>(null);
-  useFrame(() => {
-    console.log("Hey, I'm executing every frame!");
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef} scale={[0.5, 0.5, 0.5]}>
-      <boxGeometry />
-      <meshBasicMaterial color="royalblue" />
-    </mesh>
-  );
-}
 
 const Home: NextPage = () => {
   return (
     <div className="container">
-      {" "}
       <Canvas>
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
