d7685e8105e8917bf8f5e88a0362f877ce57eaf5
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-11-30 17:06:24 +0500

    created 3d space

--- components/AnimatedBox.tsx
+++ components/AnimatedBox.tsx
@@ -22,6 +22,7 @@ const AnimatedBox = ({ isTesting }: Props) => {
     // ** A Mesh is a basic scene object in three.js, and it's used to hold the geometry and the material needed to represent a shape in 3D space.
     <mesh ref={meshRef} scale={[0.5, 0.5, 0.5]}>
       {/* To create a cube, we need a BoxGeometry. This is an object that contains all the points (vertices) and fill (faces) of the cube */}
+      {/* // Checkout out threejs geometries and use them in camel case */}
       <boxGeometry />
       {/* to add standard material added to the box added aboved */}
       <meshStandardMaterial />
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,44 @@ import { Canvas, useThree } from "@react-three/fiber";
 import AnimatedBox from "../components/AnimatedBox";
 import CameraOrbitController from "../components/CameraOrbitController";
 import { OrbitControls, Stats } from "@react-three/drei";
+import { useTexture } from "@react-three/drei/core";
+
+const TexturedSpheres = () => {
+  // const map = useTexture("./textures/metal_plate_diff_1k.png");
+  const displacementMap = useTexture("./textures/metal_plate_disp_1k.png");
+  const normalMap = useTexture("./textures/metal_plate_nor_gl_1k.png");
+  const roughnessMap = useTexture("./textures/metal_plate_arm_1k.png");
+
+  return (
+    <>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[-1, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial map={displacementMap} />
+      </mesh>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh scale={[0.5, 0.5, 0.5]} position={[1, 0, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial />
+      </mesh>
+    </>
+  );
+};
 const Home: NextPage = () => {
   const testing = true;
   return (
     <div className="container">
-      <Canvas
-      // camera={{
-      //   fov: 75,
-      //   near: 2,
-      //   far: 10,
-      // }}
-      >
+      <Canvas>
         <Stats />
         {testing && <Stats />}
         {testing && <axesHelper args={[2]} />}
         {testing && <gridHelper args={[10, 10]} />}
         <OrbitControls />
-        <ambientLight intensity={0.1} />
+        <ambientLight intensity={0.2} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          {/* Checkout out threejs geometries and use them in camel case */}
-          <coneGeometry />
-          <meshStandardMaterial color={"blue"} wireframe />
-        </mesh>
-        {/* <AnimatedBox isTesting={testing} /> */}
+        <TexturedSpheres />
       </Canvas>
     </div>
   );
