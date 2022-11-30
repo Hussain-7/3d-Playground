9cee55dba1cfc2352a3159c4f827ab2afeb458b8
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-11-30 19:53:31 +0500

    feat: textures added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import { Canvas, useThree } from "@react-three/fiber";
 import AnimatedBox from "../components/AnimatedBox";
 import CameraOrbitController from "../components/CameraOrbitController";
 import { OrbitControls, Stats } from "@react-three/drei";
-import { useTexture } from "@react-three/drei/core";
+import { useTexture } from "@react-three/drei";
 
 const TexturedSpheres = () => {
-  // const map = useTexture("./textures/metal_plate_diff_1k.png");
+  const map = useTexture("./textures/metal_plate_diff_1k.png");
   const displacementMap = useTexture("./textures/metal_plate_disp_1k.png");
   const normalMap = useTexture("./textures/metal_plate_nor_gl_1k.png");
   const roughnessMap = useTexture("./textures/metal_plate_arm_1k.png");
@@ -15,15 +15,25 @@ const TexturedSpheres = () => {
     <>
       <mesh scale={[0.5, 0.5, 0.5]} position={[-1, 0, 0]}>
         <sphereGeometry />
-        <meshStandardMaterial map={displacementMap} />
+        <meshStandardMaterial map={map} />
       </mesh>
       <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
         <sphereGeometry />
-        <meshStandardMaterial />
+        <meshStandardMaterial
+          map={map}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+        />
       </mesh>
       <mesh scale={[0.5, 0.5, 0.5]} position={[1, 0, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial />
+        <sphereGeometry args={[1, 200, 200]} />
+        <meshStandardMaterial
+          map={map}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          displacementMap={displacementMap}
+          displacementScale={0.05}
+        />
       </mesh>
     </>
   );
@@ -38,8 +48,8 @@ const Home: NextPage = () => {
         {testing && <axesHelper args={[2]} />}
         {testing && <gridHelper args={[10, 10]} />}
         <OrbitControls />
-        <ambientLight intensity={0.2} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight color="white" position={[0, 0, 5]} />
         <TexturedSpheres />
       </Canvas>
     </div>
